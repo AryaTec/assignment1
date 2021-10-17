5d1926c7c108d709938aa046a3e49a9e7f71ff0c
AryaTec <90822865+AryaTec@users.noreply.github.com>
2021-10-18 00:08:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+function myFunction(){
+const table = document.getElementById("my table");
+const row = table.insertRow(1);
+const cell1 = row.insertCell(0);
+const cell2 = row.insertCell(1);
+const cell3 = row.insertCell(2);
+const cell4 = row.insertCell(3);
+const cell5 = row.insertCell(4);
+cell1.innerHTML = "0";
+cell2.innerHTML = "Neha";
+cell3.innerHTML = "kamat";
+cell4.innerHTML = "Margao";
+cell5.innerHTML = "Goa";
+}
+
+function myFunction1(){
+    const table = document.getElementById("my table");
+    const row = table.insertRow(5);
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    const cell3 = row.insertCell(2);
+    const cell4 = row.insertCell(3);
+    const cell5 = row.insertCell(4);
+    cell1.innerHTML = "5";
+    cell2.innerHTML = "Neha";
+    cell3.innerHTML = "kamat";
+    cell4.innerHTML = "Margao";
+    cell5.innerHTML = "Goa";
+    }
+    
